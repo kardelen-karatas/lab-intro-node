b52661530e723c27e12bfbc0d6820d3f295818a0
kardelen-karatas <kardelen.karatas@gmail.com>
2020-08-06 21:33:15 +0200

    done iteration 5

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos]){
+      return this.items[pos];
+    }else{
+      throw new Error('OutOfBOunds');
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 0){
+      this.items.sort((a,b)=> a-b)
+      return this.items[this.items.length-1]
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 0){
+      this.items.sort((a,b)=> a-b)
+      return this.items[0]
+    }else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
   sum() {}
 
